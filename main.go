fb68cb824e8ff8139796f3a8b024d521e1f324e9
Mattia Meleleo <melmat@tuta.io>
2021-05-11 16:12:41 +0200

    Update ingresses

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -23,14 +24,10 @@ var indexT string
 type tData struct {
 	Picture string
 	Data    map[string]map[string][]string
+	Updated time.Time
 }
 
 func main() {
-	var (
-		ctx  = context.TODO()
-		data = make(map[string]map[string][]string)
-	)
-
 	t, err := template.New("webpage").Parse(indexT)
 	if err != nil {
 		log.Fatalf("parse template: %v", err)
@@ -49,37 +46,62 @@ func main() {
 		log.Fatalf("create clientset: %v", err)
 	}
 
+	td := tData{
+		Picture: base64.StdEncoding.EncodeToString(picture),
+		Data:    make(map[string]map[string][]string),
+	}
+	update(cs, &td)
+
+	ticker := time.NewTicker(30 * time.Second)
+	quit := make(chan struct{}, 1)
+	go func(cs *kubernetes.Clientset, td *tData) {
+		for {
+			select {
+			case <-ticker.C:
+				log.Println("updating...")
+				update(cs, td)
+			case <-quit:
+				return
+			}
+		}
+	}(cs, &td)
+
+	http.HandleFunc("/", handler(t, &td))
+	log.Println("listening on http://0.0.0.0:8000")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		quit <- struct{}{}
+		log.Fatal(err)
+	}
+}
+
+func handler(t *template.Template, td *tData) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t.Execute(w, td)
+	}
+}
+
+func update(cs *kubernetes.Clientset, td *tData) {
+	ctx := context.Background()
+
 	nss, err := cs.CoreV1().Namespaces().List(ctx, metav1.ListOptions{})
 	if err != nil {
-		log.Fatalf("get namespaces: %v", err)
+		log.Printf("get namespaces: %v\n", err)
+		return
 	}
 	for _, ns := range nss.Items {
-		data[ns.Name] = make(map[string][]string)
+		td.Data[ns.Name] = make(map[string][]string)
 
 		igs, err := cs.NetworkingV1().Ingresses(ns.Name).List(ctx, metav1.ListOptions{})
 		if err != nil {
-			log.Fatalf("get ingresses for namespace %s: %v", ns.Name, err)
+			log.Printf("get ingresses for namespace %s: %v\n", ns.Name, err)
+			return
 		}
 
 		for _, ig := range igs.Items {
 			for _, rule := range ig.Spec.Rules {
-				data[ns.Name][ig.Name] = append(data[ns.Name][ig.Name], rule.Host)
+				td.Data[ns.Name][ig.Name] = append(td.Data[ns.Name][ig.Name], rule.Host)
 			}
 		}
 	}
-
-	d := tData{
-		Picture: base64.StdEncoding.EncodeToString(picture),
-		Data:    data,
-	}
-
-	http.HandleFunc("/", handler(t, d))
-	log.Println("listening on http://0.0.0.0:8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
-}
-
-func handler(t *template.Template, d tData) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		t.Execute(w, d)
-	}
+	td.Updated = time.Now()
 }
